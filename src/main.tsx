1adc895bba6e8695bd371ef8dbafa9dfb4a866e9
llamiib0t <max774433@gmail.com>
2024-03-30 20:15:29 +0900

    dsda

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,9 @@ import { Web3ModalProvider } from '@components/providers/Web3ModalProvider';
 import App from './App';
 
 import './index.css';
-// dsd
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
     <Web3ModalProvider>
         <App />,
